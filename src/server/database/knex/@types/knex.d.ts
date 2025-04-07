3883262c56919dd841ad9dae74c33b9241f88461
Yago Santos Gigeck <yago0901@gmail.com>
2025-04-07 16:38:25 -0300

    feat:added knex and configuration for 3 environments developer, test and prod

---
+++ src/server/database/knex/@types/knex.d.ts
@@ -0,0 +1,10 @@
+
+
+declare module 'knex/types/tables' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    interface Tables {
+       //cidade: ICidade 
+       //pessoa: IPessoa 
+       //usuario: IUsuario 
+    }
+}
